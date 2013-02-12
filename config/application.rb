93eb4b5eb7bbaf777383353386d15cfe5512c28c
Kris Hemenway <krishemenway@gmail.com>
2013-02-11 20:08:13 -0800

    missed configuration for assets recompilation

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,5 @@ module Movies
 
     config.sass.load_paths << File.expand_path('../../lib/assets/stylesheets/')
     config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
-
-    config.assets.initialize_on_precompile = false
   end
 end

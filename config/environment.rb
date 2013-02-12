0fe858565432302770e35b4b73514439bc066f86
Kris Hemenway <krishemenway@gmail.com>
2013-02-11 23:26:25 -0800

    i hate s3 right now

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Movies::Application.initialize!
+
+AWS::S3::DEFAULT_HOST.replace "s3-website-us-west-2.amazonaws.com"
